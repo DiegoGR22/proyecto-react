190c939699c5d51ae5516b95be9659c9fc717db9
Diego Guerrero <guerrero-romero-diego@hotmail.com>
2024-01-24 13:13:23 -0600

    Se agregó el navbar con el logo

---
+++ src/components/Logo/logo.jsx
@@ -0,0 +1,12 @@
+// import React from 'react'
+import './logo.css';
+
+const Logo = () => {
+  return (
+    <div>
+        <img src="../public/assets/img/LOGO P&B.png" alt="logo-pb" />
+    </div>
+  )
+}
+
+export default Logo
