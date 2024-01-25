ee23ed7a1e48de4107d41fbe765ca01a2f70e730
Diego Guerrero <guerrero-romero-diego@hotmail.com>
2024-01-24 19:20:58 -0600

    [FIX] Ruta de img logo

--- src/components/Logo/logo.jsx
+++ src/components/Logo/logo.jsx
@@ -1,11 +1,13 @@
 // import React from 'react'
 import './logo.css';
+import SneakHeavenLogo from '../../../public/assets/img/SneakHeaven.png';
 
 const Logo = () => {
   return (
     <div>
         {/* <img src="../public/assets/img/Sneak Heaven.png" alt="logo-sh" /> */}
-        <img src="../../../public/assets/img/SneakHeaven.png" alt="logo-sh" />
+        {/* <img src="../../../public/assets/img/SneakHeaven.png" alt="logo-sh" /> */}
+        <img src={SneakHeavenLogo} alt="logo-sh" />
     </div>
   )
 }
