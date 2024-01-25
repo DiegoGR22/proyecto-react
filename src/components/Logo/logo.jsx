e5a120d25cc4409e26d657d9a217cb095a5411db
Diego Guerrero <guerrero-romero-diego@hotmail.com>
2024-01-24 18:58:00 -0600

    [FIX] src del logo

--- src/components/Logo/logo.jsx
+++ src/components/Logo/logo.jsx
@@ -4,7 +4,8 @@ import './logo.css';
 const Logo = () => {
   return (
     <div>
-        <img src="../public/assets/img/Sneak Heaven.png" alt="logo-sh" />
+        {/* <img src="../public/assets/img/Sneak Heaven.png" alt="logo-sh" /> */}
+        <img src="../../../public/assets/img/Sneak Heaven.png" alt="logo-sh" />
     </div>
   )
 }
