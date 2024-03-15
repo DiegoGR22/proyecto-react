f219558737c62aa4f408c11c51b6845c924ab736
DiegoGR22 <guerrero-romero-diego@hotmail.com>
2024-03-14 23:17:21 -0600

    No se pudo hacer el pedido desde Firebase en el ItemDetailContainer

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import ProductosJSON from '../../db/productos.json';
 import ItemDetail from '../ItemDetail/ItemDetail';
-import { getDoc, doc, getFirestore } from 'firebase/firestore';
+// import { getDoc, doc, getFirestore } from 'firebase/firestore';
 // import { db } from "../firebase/config";
 
 
