4403ff04b145c89ffd2b8701d8e03d55ad5f98bd
DiegoGR22 <guerrero-romero-diego@hotmail.com>
2024-03-15 19:45:24 -0600

    Validaciones del form

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -8,7 +8,7 @@ import { Link } from 'react-router-dom';
 const Checkout = () => {
 
     const { cart, totalPrice, clearAll } = useContext(CartContext);
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
     const [isPaid, setIsPaid] = useState(false);
     const [orderId, setOrderId] = useState("");
 
@@ -50,16 +50,57 @@ const Checkout = () => {
                         </Button>
                     </div>
 
-                    : 
-                    
+                    :
+
                     <div className='containerCheckout'>
 
                         <h1>Finalizar Compra</h1>
+
                         <form onSubmit={handleSubmit(createOrder)} className='containerForm'>
 
-                            <input type="text" placeholder='Ingresa tu nombre' {...register("name")} />
-                            <input type="email" placeholder='Ingresa tu e-mail' {...register("email")} />
-                            <input type="phone" placeholder='Ingresa tu telefono' {...register("phone")} />
+                            <input type="text" placeholder='Ingresa tu nombre' {...register("name", {
+                                required: {
+                                    value: true, message: "Campo obligatorio"
+                                }
+                            })} />
+                            {errors.name && <p>{errors.name.message}</p>}
+
+                            <input type="email" placeholder='Ingresa tu e-mail' {...register("email", {
+                                required: { value: true, message: "Campo obligatorio" },
+                                pattern: {
+                                    value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                                    message: "Por favor, introduce una dirección de correo electrónico válida."
+                                }
+                            })} />
+                            {errors.email && <p>{errors.email.message}</p>}
+
+                            <input
+                                type="email"
+                                placeholder='Confirma tu e-mail'
+                                {...register("confirmEmail", {
+                                    required: { value: true, message: "Campo obligatorio" },
+                                    pattern: {
+                                        value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                                        message: "Por favor, introduce una dirección de correo electrónico válida."
+                                    },
+                                    validate: (value) => value === watch("email") || "Los correos electrónicos no coinciden"
+                                })}
+                            />
+                            {errors.confirmEmail && <p>{errors.confirmEmail.message}</p>}
+
+                            <input type="phone" placeholder='Teléfono a 10 dígitos' {...register("phone", {
+                                required: { value: true, message: "Campo obligatorio" },
+                                pattern: {
+                                    value: /^\d+$/,
+                                    message: "Por favor, ingresa solo números."
+                                }
+                            })}
+                                maxLength={10} onInput={(e) => {
+                                    if (e.target.value.length > 10) {
+                                        e.target.value = e.target.value.slice(0, 10);
+                                    }
+                                }} />
+                            {errors.phone && <p>{errors.phone.message}</p>}
 
                             <Button type='submit'>Pagar</Button>
 
