df3c12c27ce3f250311adb39e40851de2eb32884
DiegoGR22 <guerrero-romero-diego@hotmail.com>
2024-03-16 20:42:04 -0600

    FIX design del Spinner

--- src/components/LoadSpinner/LoadSpinner.jsx
+++ src/components/LoadSpinner/LoadSpinner.jsx
@@ -2,11 +2,14 @@ import { Spinner } from "react-bootstrap"
 
 const LoadSpinner = () => {
     return (
-        <div>
-            <Spinner className="text-primary" animation="grow" style={{height: '5rem', width: '5rem'}}>
-                <span className="visually-hidden">Loading...</span>
-            </Spinner>
-        </div>
+        <>
+            <div className="d-flex flex-column justify-content-center align-items-center vh-100">
+                <Spinner className="text-success" style={{ height: '5rem', width: '5rem', fontSize: '2em' }}>
+                    <span className="visually-hidden">Loading...</span>
+                </Spinner>
+                <span className="d-block text-success fs-1">Loading...</span>
+            </div>
+        </>
     )
 }
 
